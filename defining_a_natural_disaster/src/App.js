819fbb6adb94628714734e91fe00b4d5b245c242
Spectre <spectrefromthefuture@gmail.com>
2023-09-23 18:36:37 +0400

    made the header layout for the site

--- defining_a_natural_disaster/src/App.js
+++ defining_a_natural_disaster/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { ReactComponent as GithubLogo } from './images/github-mark-white.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+
+        <div className="left-block">
+        <GithubLogo 
+          className="github-icon" 
+          alt="Link to my github"
+          viewBox='0 0 100 100'
+          />
+        </div>
+    
+        <div className="center-block">
+          <div className="header-name-container">
+            <p className="header-name">Sentiment analysis</p>
+            <p className="header-name header-name-glowing">Sentiment analysis</p>
+          </div>
+        </div>
+
+        <div className="right-block">
+          
+        </div>
+      </div>
     </div>
   );
 }
